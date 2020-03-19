a6a976da7e36858cf238b4aac5ff1338ac2124c9
Josh West <shuawest@gmail.com>
2020-03-18 22:27:27 -0400

    added continuity API

--- pkg/apis/broker/v2alpha1/activemqartemis_types.go
+++ pkg/apis/broker/v2alpha1/activemqartemis_types.go
@@ -11,76 +11,151 @@ import (
 // ActiveMQArtemisSpec defines the desired state of ActiveMQArtemis
 // +k8s:openapi-gen=true
 type ActiveMQArtemisSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-
-	AdminUser      string             `json:"adminUser,omitempty"`
+	// User name for standard broker user. It is required for connecting to the broker. If left empty, it will be generated.
+	AdminUser string `json:"adminUser,omitempty"`
+	// Password for standard broker user. It is required for connecting to the broker. If left empty, it will be generated.
 	AdminPassword  string             `json:"adminPassword,omitempty"`
 	DeploymentPlan DeploymentPlanType `json:"deploymentPlan,omitempty"`
-	Acceptors      []AcceptorType     `json:"acceptors,omitempty"`
-	Connectors     []ConnectorType    `json:"connectors,omitempty"`
-	Console        ConsoleType        `json:"console,omitempty"`
+	// Configuration of all acceptors
+	Acceptors []AcceptorType `json:"acceptors,omitempty"`
+	// Configuration of all connectors
+	Connectors []ConnectorType `json:"connectors,omitempty"`
+	// Configuration for the embedded web console
+	Console ConsoleType `json:"console,omitempty"`
+	// Whether or not to enable the continuity plugin. See required fields for continuity.
+	EnableContinuity bool `json:"enableContinuity,omitempty"`
+	// Name the continuity site. Must be unique in the set of peers (is same across the artemis cluster). Required for continuity.
+	SiteId string `json:"siteId,omitempty"`
+	// Username to connect to the local broker for continuity connections. Required for continuity.
+	LocalContinuityUser string `json:"localContinuityUser,omitempty"`
+	// Password to connect to the local broker for continuity connections. Required for continuity.
+	LocalContinuityPass string `json:"localContinuityPass,omitempty"`
+	// Username to connect to the peer site broker/cluster for continuity connections. Required for continuity.
+	PeerSiteUrl string `json:"peerSiteUrl,omitempty"`
+	// Username to connect to the peer site broker/cluster for continuity connections. Required for continuity.
+	PeerContinuityUser string `json:"peerContinuityUser,omitempty"`
+	// Password to connect to the peer site broker/cluster for continuity connections. Required for continuity.
+	PeerContinuityPass string `json:"peerContinuityPass,omitempty"`
+	// Identifies this is the site that should be active when first started. If another active site is connected to, this site will defer to the other. You can also start both sites inactive and explictly activate the desired start. Required for continuity.
+	ActiveOnStart bool `json:"activeOnStart,omitempty"`
+	// Size of the broker id cache size, used by the broker to remove duplicate messages across sites. Make sure the id cache is sufficiently sized for your volume of messages. The default is 3000.
+	BrokerIdCacheSize int64 `json:"brokerIdCacheSize,omitempty"`
+	// Amount of time in millseconds to delay messages in the inflow staging queues before delivering them to the target queues. Useful for active:active site topologies. The default is 60000 ms or 1 minute.
+	InflowStagingDelay int32 `json:"inflowStagingDelay,omitempty"`
+	// Bridge reconnection interval for all the bridges created by the continuity plugin. The default is 1000 ms or 1 second.
+	BridgeInterval int32 `json:"bridgeInterval,omitempty"`
+	// Bridge reconnection interval backoff multiplier for all the bridges created by the continuity plugin. The default is 0.5.
+	BridgeIntervalMultiplier float32 `json:"bridgeIntervalMultiplier,omitempty"`
+	// Time in milliseconds between polls to check for a site to be exhausted during deactivation. The default is 100 ms.
+	OutflowExhaustedPollDuration int32 `json:"outflowExhaustedPollDuration,omitempty"`
+	// Time in milliseconds between polls to all the inflow acks have been consumed during activation. The default is 100 ms.
+	InflowAcksConsumedPollDuration int32 `json:"inflowAcksConsumedPollDuration,omitempty"`
+	// Time in milliseconds to activate a site and start serving clients, overriding the wait for the peer site to be exhausted, and acks to be consumed. The default is 300000 ms or 5 minutes.
+	ActivationTimeout int64 `json:"activationTimeout,omitempty"`
+	// Whether or not to reorganized all the address, queue, divert, and bridge primitives under the continuity hierarchy in JMX/Jolokia. The default is true.
+	ReorgManagement bool `json:"reorgManagement,omitempty"`
+	// Logging level for the continuity plugin. The default is INFO.
+	// +kubebuilder:validation:Enum=TRACE,DEBUG,INFO,ERROR,FATAL
+	ContinuityLogLevel string `json:"continuityLogLevel,omitempty"`
 }
 
 type DeploymentPlanType struct {
-	Image              string `json:"image,omitempty"`
-	Size               int32  `json:"size,omitempty"`
-	RequireLogin       bool   `json:"requireLogin,omitempty"`
-	PersistenceEnabled bool   `json:"persistenceEnabled,omitempty"`
-	JournalType        string `json:"journalType,omitempty"`
-	MessageMigration   *bool  `json:"messageMigration,omitempty"`
+	// The image used for the broker deployment
+	Image string `json:"image,omitempty"`
+	// The number of broker pods to deploy
+	// +kubebuilder:validation:Maximum=16
+	// +kubebuilder:validation:Minimum=1
+	Size int32 `json:"size,omitempty"`
+	// If true require user password login credentials for broker protocol ports
+	RequireLogin bool `json:"requireLogin,omitempty"`
+	// If true use persistent volume via persistent volume claim for journal storage
+	PersistenceEnabled bool `json:"persistenceEnabled,omitempty"`
+	// If aio use ASYNCIO, if nio use NIO for journal IO
+	JournalType string `json:"journalType,omitempty"`
+	// If true migrate messages on scaledown
+	MessageMigration *bool `json:"messageMigration,omitempty"`
 }
 
 type AcceptorType struct {
-	Name                string `json:"name"`
-	Port                int32  `json:"port,omitempty"`
-	Protocols           string `json:"protocols,omitempty"`
-	SSLEnabled          bool   `json:"sslEnabled,omitempty"`
-	SSLSecret           string `json:"sslSecret,omitempty"`
+	// The name of the acceptor
+	Name string `json:"name"`
+	// Port number
+	Port int32 `json:"port,omitempty"`
+	// The protocols to enable for this acceptor
+	Protocols string `json:"protocols,omitempty"`
+	// Whether or not to enable SSL on this port
+	SSLEnabled bool `json:"sslEnabled,omitempty"`
+	// Name of the secret to use for ssl information
+	SSLSecret string `json:"sslSecret,omitempty"`
+	// Comma separated list of cipher suites used for SSL communication.
 	EnabledCipherSuites string `json:"enabledCipherSuites,omitempty"`
-	EnabledProtocols    string `json:"enabledProtocols,omitempty"`
-	NeedClientAuth      bool   `json:"needClientAuth,omitempty"`
-	WantClientAuth      bool   `json:"wantClientAuth,omitempty"`
-	VerifyHost          bool   `json:"verifyHost,omitempty"`
-	SSLProvider         string `json:"sslProvider,omitempty"`
-	SNIHost             string `json:"sniHost,omitempty"`
-	Expose              bool   `json:"expose,omitempty"`
-	AnycastPrefix       string `json:"anycastPrefix,omitempty"`
-	MulticastPrefix     string `json:"multicastPrefix,omitempty"`
-	ConnectionsAllowed  int    `json:"connectionsAllowed,omitempty"`
+	// Comma separated list of protocols used for SSL communication.
+	EnabledProtocols string `json:"enabledProtocols,omitempty"`
+	// Tells a client connecting to this acceptor that 2-way SSL is required. This property takes precedence over wantClientAuth.
+	NeedClientAuth bool `json:"needClientAuth,omitempty"`
+	// Tells a client connecting to this acceptor that 2-way SSL is requested but not required. Overridden by needClientAuth.
+	WantClientAuth bool `json:"wantClientAuth,omitempty"`
+	// The CN of the connecting client's SSL certificate will be compared to its hostname to verify they match. This is useful only for 2-way SSL.
+	VerifyHost bool `json:"verifyHost,omitempty"`
+	// Used to change the SSL Provider between JDK and OPENSSL. The default is JDK.
+	SSLProvider string `json:"sslProvider,omitempty"`
+	// A regular expression used to match the server_name extension on incoming SSL connections. If the name doesn't match then the connection to the acceptor will be rejected.
+	SNIHost string `json:"sniHost,omitempty"`
+	// Whether or not to expose this acceptor
+	Expose bool `json:"expose,omitempty"`
+	// To indicate which kind of routing type to use.
+	AnycastPrefix string `json:"anycastPrefix,omitempty"`
+	// To indicate which kind of routing type to use.
+	MulticastPrefix string `json:"multicastPrefix,omitempty"`
+	// Limits the number of connections which the acceptor will allow. When this limit is reached a DEBUG level message is issued to the log, and the connection is refused.
+	ConnectionsAllowed int `json:"connectionsAllowed,omitempty"`
 }
 
 type ConnectorType struct {
-	Name                string `json:"name"`
-	Type                string `json:"type,omitempty"`
-	Host                string `json:"host"`
-	Port                int32  `json:"port"`
-	SSLEnabled          bool   `json:"sslEnabled,omitempty"`
-	SSLSecret           string `json:"sslSecret,omitempty"`
+	// The name of the acceptor
+	Name string `json:"name"`
+	// The type either tcp or vm
+	Type string `json:"type,omitempty"`
+	// Hostname or IP to connect to
+	Host string `json:"host"`
+	// Port number
+	Port int32 `json:"port"`
+	// Whether or not to enable SSL on this port
+	SSLEnabled bool `json:"sslEnabled,omitempty"`
+	// Name of the secret to use for ssl information
+	SSLSecret string `json:"sslSecret,omitempty"`
+	// Comma separated list of cipher suites used for SSL communication.
 	EnabledCipherSuites string `json:"enabledCipherSuites,omitempty"`
-	EnabledProtocols    string `json:"enabledProtocols,omitempty"`
-	NeedClientAuth      bool   `json:"needClientAuth,omitempty"`
-	WantClientAuth      bool   `json:"wantClientAuth,omitempty"`
-	VerifyHost          bool   `json:"verifyHost,omitempty"`
-	SSLProvider         string `json:"sslProvider,omitempty"`
-	SNIHost             string `json:"sniHost,omitempty"`
-	Expose              bool   `json:"expose,omitempty"`
+	// Comma separated list of protocols used for SSL communication.
+	EnabledProtocols string `json:"enabledProtocols,omitempty"`
+	// Tells a client connecting to this acceptor that 2-way SSL is required. This property takes precedence over wantClientAuth.
+	NeedClientAuth bool `json:"needClientAuth,omitempty"`
+	//Tells a client connecting to this acceptor that 2-way SSL is requested but not required. Overridden by needClientAuth.
+	WantClientAuth bool `json:"wantClientAuth,omitempty"`
+	// The CN of the connecting client's SSL certificate will be compared to its hostname to verify they match. This is useful only for 2-way SSL.
+	VerifyHost bool `json:"verifyHost,omitempty"`
+	// Used to change the SSL Provider between JDK and OPENSSL. The default is JDK.
+	SSLProvider string `json:"sslProvider,omitempty"`
+	// A regular expression used to match the server_name extension on incoming SSL connections. If the name doesn't match then the connection to the acceptor will be rejected.
+	SNIHost string `json:"sniHost,omitempty"`
+	// Whether or not to expose this connector
+	Expose bool `json:"expose,omitempty"`
 }
 
 type ConsoleType struct {
-	Expose        bool   `json:"expose,omitempty"`
-	SSLEnabled    bool   `json:"sslEnabled,omitempty"`
-	SSLSecret     string `json:"sslSecret,omitempty"`
-	UseClientAuth bool   `json:"useClientAuth,omitempty"`
+	// Whether or not to expose this port
+	Expose bool `json:"expose,omitempty"`
+	// Whether or not to enable SSL on this port
+	SSLEnabled bool `json:"sslEnabled,omitempty"`
+	// Name of the secret to use for ssl information
+	SSLSecret string `json:"sslSecret,omitempty"`
+	// If the embedded server requires client authentication
+	UseClientAuth bool `json:"useClientAuth,omitempty"`
 }
 
 // ActiveMQArtemisStatus defines the observed state of ActiveMQArtemis
 // +k8s:openapi-gen=true
 type ActiveMQArtemisStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 	PodStatus olm.DeploymentStatus `json:"podStatus"`
 }
 
